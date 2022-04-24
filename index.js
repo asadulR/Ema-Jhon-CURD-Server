6a84b2501de71e3918474e83f661769f1275a34b
asadulR <asad1252001@gmail.com.com>
2022-04-24 09:28:52 +0600

    connected to mongodb database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require('cors');
-const port = precess.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
@@ -12,18 +13,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3est.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try{
+
+    }
+
+    finally{
+
+        
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Ema Jhon is running.')
 });
-
-
-
-
-
-
-
-
-
 
 
 app.listen(port, () => {
